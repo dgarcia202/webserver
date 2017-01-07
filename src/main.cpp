8b8c12a84243e4fe00606814dbf6c735f3737ea5
Diego <you@example.com>
2017-01-07 23:42:55 +0100

    initial command parameter check

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <csignal>
 #include <stdlib.h>
+#include <string>
+#include <regex>
 
 #include "webserver.h"
 #include "stringtools.h"
@@ -15,8 +17,31 @@ void signalHandler(int signal)
 	exit(signal);
 }
 
-int main()
+bool IsPortNumber(std::string literal)
 {
+	std::regex expr("^\\d{1,5}$");
+	return std::regex_match(literal, expr);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc > 2)
+	{
+		std::cerr << "error: incorrect number of arguments";
+		exit(1);
+	}
+
+	int port = 8886;
+	/*if (argc == 2)
+	{
+		if (!IsPortNumber(std::string(argv[1])))
+		{
+			std::cerr << "error: incorrect argument format";
+			exit(1);
+		}
+		port = std::stoi(std::string(argv[1]));
+	}*/
+
 	signal(SIGINT, signalHandler);
 
 	HomeControllerFactory home_ctlr_factory;
@@ -25,5 +50,5 @@ int main()
 	QueryStringControllerFactory query_string_ctrl_factory;
 	dynws::WebServer::router_.RegisterRoute("^\\/customers\\?id=(.*)$", &query_string_ctrl_factory);
 
-	server.Start(8886);
+	server.Start(port);
 }
